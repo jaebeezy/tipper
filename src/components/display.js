40eff0aea4343dd8e0c7cb01fba6d1cd71b60ef7
Jae Young Choi <jaeyoung@Jaes-Mac-mini.home>
2021-10-02 13:44:33 -0400

    fixed display component overflow

--- src/components/display.js
+++ src/components/display.js
@@ -3,13 +3,24 @@ import React from "react";
 import { round } from "../helpers/round";
 
 const Display = ({ label, amount, people }) => {
+  // const displayHelper = () => {
+  //   let answer = round(amount / people);
+  //   console.log(answer);
+
+  //   if (answer === "NaN") {
+  //     return "N/A";
+  //   } else {
+  //     return `$${answer}`;
+  //   }
+  // };
+
   return (
     <div className="display-container">
       <div className="per-person">
         <h5 className="display-header">{label}</h5>
         <h6>/ person</h6>
       </div>
-      <div>
+      <div class="amount-container">
         <h1 className="amount-header">${round(amount / people)}</h1>
       </div>
     </div>
